97a9cb76ad5a27f9209dcb7323f99d981a0dcfa5
Iklakh, Shekh <shekh_iklakh@uhc.com>
2021-01-12 15:40:40 +0530

    winium

---
+++ Winium-master/dotnet/src/OpenQA.Selenium.Winium/WiniumDriverCommandExecutor.cs
@@ -0,0 +1,103 @@
+﻿namespace OpenQA.Selenium.Winium
+{
+    #region using
+
+    using System;
+    using System.Reflection;
+
+    using OpenQA.Selenium.Remote;
+
+    #endregion
+
+    public class WiniumDriverCommandExecutor : ICommandExecutor
+    {
+        #region Fields
+
+        private ICommandExecutor internalExecutor;
+
+        private WiniumDriverService service;
+        private bool disposedValue;
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public WiniumDriverCommandExecutor(WiniumDriverService driverService, TimeSpan commandTimeout)
+        {
+            this.service = driverService;
+            this.internalExecutor = CommandExecutorFactory.GetHttpCommandExecutor(driverService.ServiceUrl, commandTimeout);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public CommandInfoRepository CommandInfoRepository
+        {
+            get
+            {
+                return this.internalExecutor.CommandInfoRepository;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public Response Execute(Command commandToExecute)
+        {
+            if (commandToExecute == null)
+            {
+                throw new ArgumentNullException("commandToExecute", "Command may not be null");
+            }
+
+            if (commandToExecute.Name == DriverCommand.NewSession)
+            {
+                this.service.Start();
+            }
+
+            try
+            {
+                return this.internalExecutor.Execute(commandToExecute);
+            }
+            finally
+            {
+                if (commandToExecute.Name == DriverCommand.Quit)
+                {
+                    this.service.Dispose();
+                }
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~WiniumDriverCommandExecutor()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
